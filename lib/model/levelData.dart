2b0a36e951ed91677036ae4a3d5caad58b74ba77
Ekrem Kamaz <ekrem.kamaz@yandex.com>
2021-02-27 00:47:21 +0300

    Changed default snake size

--- lib/model/levelData.dart
+++ lib/model/levelData.dart
@@ -7,8 +7,9 @@ class LevelData {
     switch (level) {
       case 2:
         board = List.generate(20, (index) => List.generate(20, (index) => 0));
-        board[5][5] = 1;
-        head = {'x': 5, 'y': 5, 'score': 1};
+        board[5][5] = 2;
+        board[4][5] = 1;
+        head = {'x': 5, 'y': 5, 'score': 2};
 
         board[15][5] = -2;
         board[5][15] = -2;
@@ -16,12 +17,15 @@ class LevelData {
           '15,5': '5,15',
           '5,15': '15,5',
         };
+
         return board;
       case 1:
       default:
         board = List.generate(20, (index) => List.generate(20, (index) => 0));
-        board[5][5] = 1;
-        head = {'x': 5, 'y': 5, 'score': 1};
+        board[5][5] = 2;
+        board[4][5] = 1;
+        head = {'x': 5, 'y': 5, 'score': 2};
+        
         return board;
     }
   }
